23715841a25da0d2c702a17f15e23e70fcce5483
aidan-h <aidanhammond2003@gmail.com>
2024-02-14 08:47:49 -0600

    Added TailwindCSS

--- nvim/init.lua
+++ nvim/init.lua
@@ -87,23 +87,52 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 -- Setup language servers.
 local lspconfig = require('lspconfig')
 
-lspconfig.pyright.setup{
-  capabilities = capabilities
-}
+local servers = [
+	"pyright",
+	"tailwindcss",
+	"luau_lsp",
+	"tsserver",
+]
 
-lspconfig.marksman.setup{
-  capabilities = capabilities
-}
+for i, server in ipairs(servers) do
+	lspconfig[server].setup {
+		capabilities = capabilities
+	}
+end
 
-lspconfig.luau_lsp.setup{
-  capabilities = capabilities
-}
+lspconfig.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            }
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          }
+        }
+      })
 
-lspconfig.tsserver.setup {
-  capabilities = capabilities
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end
 }
 
 lspconfig.eslint.setup({
+	capabilities = capabilities
   on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
@@ -163,7 +192,6 @@ local has_words_before = function()
 end
 
 local cmp = require'cmp'
-
   cmp.setup({
     snippet = {
       -- REQUIRED - you must specify a snippet engine
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -104,6 +104,11 @@ _G.packer_plugins = {
     path = "C:\\Users\\ahamm\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
+  neoformat = {
+    loaded = true,
+    path = "C:\\Users\\ahamm\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\neoformat",
+    url = "https://github.com/sbdchd/neoformat"
+  },
   ["nvim-cmp"] = {
     loaded = true,
     path = "C:\\Users\\ahamm\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-cmp",
